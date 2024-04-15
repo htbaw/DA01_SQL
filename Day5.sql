fb2629e73f24e409a82874d4e942bce2588211ce
htbaw <166812910+htbaw@users.noreply.github.com>
2024-04-15 13:06:17 +0700

    Create Day5.sql

---
+++ Day5.sql
@@ -0,0 +1,33 @@
+--baitap1
+SELECT DISTINCT CITY FROM STATION
+WHERE ID%2=0
+
+--baitap2
+Select COUNT(CITY) - COUNT(Distinct CITY) 
+From STATION
+
+--baitap3
+
+--baitap4
+
+--baitap5
+
+--baitap6
+
+--baitap7
+
+--baitap8
+
+--baitap9
+
+--baitap10
+
+--baitap11
+
+--baitap12
+
+--baitap13
+
+--baitap14
+
+--baitap15
