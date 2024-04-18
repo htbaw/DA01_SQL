7d791c10d34865f5988b0e303d85f60a7aa35f39
htbaw <166812910+htbaw@users.noreply.github.com>
2024-04-18 21:32:27 +0700

    Update Day7.sql

--- Day7.sql
+++ Day7.sql
@@ -44,8 +44,13 @@ FROM Tweets
 WHERE LENGTH(content) > 15
 
 --baitap7
-
-
+SELECT 
+activity_date AS day,
+COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
+GROUP BY 1
+  
 --baitap8
 select 
 COUNT (employee_id) AS number_employee
